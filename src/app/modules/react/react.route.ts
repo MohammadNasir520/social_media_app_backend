225789b7684fc3dfd85c090983fff776279154ea
MohammadNasir520 <nasirahsan520@gmail.com>
2023-07-09 16:08:00 +0600

    react count to post done

--- src/app/modules/react/react.route.ts
+++ src/app/modules/react/react.route.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import { ReactController } from './react.controller';
 const router = express.Router();
 
-router.post('/create-react', ReactController.createReact);
+router.put('/create-react', ReactController.createReact);
 
 router.get('/:id', ReactController.getAllReactsOfSinglePost);
 router.get('/', ReactController.getSingleReact);
--- src/app/modules/react/react.service.ts
+++ src/app/modules/react/react.service.ts
@@ -1,11 +1,35 @@
+import { Post } from '../post/post.model';
 import { IReact } from './react.interface';
 import { React } from './react.model';
 
-const createReact = async (ReactData: IReact): Promise<IReact> => {
-  const createReact = (
-    await (await React.create(ReactData)).populate('user')
-  ).populate('post');
-  return createReact;
+const createReact = async (ReactData: IReact): Promise<IReact | undefined> => {
+  const findReact = await React.find({
+    post: ReactData.post,
+    user: ReactData.user,
+  });
+
+  console.log('find reac', findReact);
+  if (findReact.length <= 0) {
+    const createReact = await (
+      await (await React.create(ReactData)).populate('user')
+    ).populate('post');
+
+    if (createReact) {
+      const findLastRectNumber = await Post.findOne(
+        { _id: ReactData.post },
+        { react: 1 }
+      );
+      const totalReact = Number(findLastRectNumber?.react) + 1 || 1;
+      const findPostAndUpdateReact = await Post.findOneAndUpdate(
+        { _id: ReactData.post },
+        { react: totalReact },
+        { upsert: true, new: true }
+      );
+      console.log(findPostAndUpdateReact);
+    }
+
+    return createReact;
+  }
 };
 
 const getAllReactsOfSinglePost = async (postId: string) => {
